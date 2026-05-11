fa2c4a886d4e72a9dd401f04a094fafcd68aab89
Juan Avendaño <jpavendanb@eafit.edu.co>
2026-05-11 01:19:27 -0500

    chore(docker, config): configure requried rendering modes and create dockerfile to deploy app

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,14 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { articles } from './shared/data/article.data';
+
+const blogPrerenderRoutes = ['/blog', ...articles.map((article) => `/blog/${article.slug}`)];
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  nitro: {
+    preset: 'node-server',
+    prerender: {
+      routes: blogPrerenderRoutes,
+    },
+  },
   app: { head: { htmlAttrs: { lang: 'en' } } },
   routeRules: {
     '/computers': { ssr: true },
     '/computers/**': { ssr: true },
-    '/blog': { ssr: true },
-    '/blog/**': { ssr: true },
+    '/blog': { prerender: true },
+    '/blog/**': { prerender: true },
     '/tools': { ssr: false },
     '/tools/**': { ssr: false },
   },
